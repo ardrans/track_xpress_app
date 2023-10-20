6a38d9d64c31d38501741d5fc8dde49072d82471
ardrans <nsardra@gmail.com>
2023-10-20 15:56:23 +0530

    database configuration amnd models created

--- track_xpress/settings.py
+++ track_xpress/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+#from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'track_xpress_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'track_xpress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'track_xpress_app',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- track_xpress_app/models.py
+++ track_xpress_app/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
+class Drivers(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='track_xpress_users')
+    user_permissions = models.ManyToManyField(Permission, related_name='track_xpress_users')
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=15)
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone_number']
+    email_verified = models.BooleanField(default=False)
+    phone_number_verified = models.BooleanField(default=False)
+
+class Customers(models.Model):
+    driver_id = models.ForeignKey(Drivers, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
